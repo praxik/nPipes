084f6255bf32824eabdd3a2f8be9cef61dbf94d1
Penn Taylor <rpenn3@gmail.com>
2018-09-12 16:28:01 -0500

    Not-quite-tests for ezq message conversion

--- tests/ezqconverterTests.py
+++ tests/ezqconverterTests.py
@@ -3,7 +3,8 @@
 import unittest
 import time
 # from npipes.message.ezqconverter import *
-from npipes.message.header import Message, convertFromEZQ
+from npipes.message.header import Message
+from npipes.message.ezqconverter import convertFromEZQ, convertToEZQ
 
 class EZQConverterTestCase(unittest.TestCase):
 
@@ -26,6 +27,13 @@ class EZQConverterTestCase(unittest.TestCase):
             msgstr = msgfile.read()
             with convertFromEZQ(Message, msgstr) as msg:
                 print(msg._toMinDict())
+
+    def test_convertToEZQFullMsg(self):
+        with open("tests/ezq/ezq.msg") as msgfile:
+            msgstr = msgfile.read()
+            with convertFromEZQ(Message, msgstr) as msg:
+                print(convertToEZQ(msg))
+
 
 
 if __name__ == '__main__':
