2c2fddc4e4ba9f001c1789de3892788e11464ce6
Penn Taylor <rpenn3@gmail.com>
2018-10-03 16:43:59 -0500

    Initial cut of sns trigger

--- npipes/triggers/sns.py
+++ npipes/triggers/sns.py
@@ -2,6 +2,19 @@
 
 from ..message.header import Message
 from ..outcome import Outcome, Success, Failure
+from ..message.ezqconverter import toEzqOrJsonLines
+
+import boto3
 
 def sendMessage(topic, message:Message) -> Outcome[str, None]:
-    pass
+    """Publishes message to topic.
+
+    Does not check message size to ensure it will fit within SNS's
+    restrictions.
+    """
+    sns = boto3.client("sns")
+    resp = sns.publish(TopicArn=topic,
+                       Message=toEzqOrJsonLines(message))
+    # SNS doesn't return a success or failure code in the response, so
+    # we always treat it as success  :|
+    return Success(None)
